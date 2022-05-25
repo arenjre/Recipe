3debbbcd7a994b6766942eebae8c26e4b6415ba4
Deependra Singh <>
2022-05-25 15:37:30 +0530

    docker file changed !

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -5,18 +5,20 @@ from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 import time
 
+
 class Command(BaseCommand):
     """Command"""
+
     def handle(self, *args, **options):
         """Entry point for command"""
         self.stdout.write("wait_for_database")
         db_up = False
         while db_up is False:
             try:
-                self.check(databases=['default'])
+                self.check(databases=["default"])
                 db_up = True
             except (Psycopg2OpError, OperationalError):
                 self.stdout.write("Database unavailable, waiting 1 second...")
                 time.sleep(1)
-        
+
         self.stdout.write(self.style.SUCCESS("database available"))
